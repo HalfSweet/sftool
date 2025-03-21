d16a991c8fb1c1779e4741b0f8bdd1b5db00e348
HalfSweet <halfsweet@halfsweet.cn>
2025-03-22 00:45:03 +0800

    fix:

--- install_deb.sh
+++ install_deb.sh
@@ -18,7 +18,7 @@ apt-get install --assume-yes --no-install-recommends \
 
 # get our debian sources
 debsource="deb http://deb.debian.org/debian/ bookworm main"
-debsource="${debsource}\ndeb http://security.debian.org/debian-security bookworm-security main"
+debsource="${debsource}\ndeb https://security.debian.org/debian-security bookworm-security main"
 
 # temporarily use debian sources rather than ubuntu.
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
@@ -28,10 +28,10 @@ dpkg --add-architecture "${arch}" || echo "foreign-architecture ${arch}" \
   > /etc/dpkg/dpkg.cfg.d/multiarch
 
 # Add Debian keys.
-curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{7.0,8,9,10}.asc' -O
-curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{8,9,10}-security.asc' -O
-curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/release-{7,8,9,10}.asc' -O
-curl --retry 3 -sSfL 'https://www.ports.debian.org/archive_{2020,2021,2022}.key' -O
+curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{7.0,8,9,10,11,12}.asc' -O
+curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{8,9,10,11,12}-security.asc' -O
+curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/release-{7,8,9,10,11,12}.asc' -O
+curl --retry 3 -sSfL 'https://www.ports.debian.org/archive_{2020,2021,2022,2023,2024}.key' -O
 
 for key in *.asc *.key; do
   apt-key add "${key}"
